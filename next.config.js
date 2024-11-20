bb11fbe67a1199bad2a1d6d39eefd240d19b732c
Onur Bozkurt <onurbozkurt@gmail.com>
2024-11-20 11:13:49 +0300

    Use esmodules in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  experimental: {
+    serverActions: true,
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
